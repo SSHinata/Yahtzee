6662018c06036827a35deced49dc6a11f15a3c5c
pitaya <peixing@baoflag.com>
2026-02-04 14:01:48 +0800

    Add cloudfunction

---
+++ cloudfunctions/ping/index.js
@@ -0,0 +1,7 @@
+exports.main = async (event, context) => {
+  return {
+    ok: true,
+    message: 'pong',
+    event
+  }
+}
--- minigame/game.js
+++ minigame/game.js
@@ -1,3 +1,21 @@
 import Main from './js/main';
 
+if (wx && wx.cloud && typeof wx.cloud.init === 'function') {
+  wx.cloud.init({
+    env: 'cloud1-2g2pn7fvfec40ab5',
+    traceUser: true
+  });
+
+  if (typeof wx.cloud.callFunction === 'function') {
+    wx.cloud.callFunction({
+      name: 'ping',
+      data: { t: Date.now() }
+    }).then((res) => {
+      console.log('[cloud] ping ok:', res && res.result);
+    }).catch((err) => {
+      console.error('[cloud] ping failed:', err);
+    });
+  }
+}
+
 new Main();
